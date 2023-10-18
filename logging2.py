6b5eb9d7b0cccc58b076fbd2acc480a2d85dc21a
AG123-321 <123011074+AG123-321@users.noreply.github.com>
2023-10-18 14:43:59 +0000

    modified:   logging2.py

--- logging2.py
+++ logging2.py
@@ -2,17 +2,17 @@
 import termcolor
 color = termcolor.colored
 def info(msg):
-    print(color("INFO: ", "blue", attrs=['bold']), end='')
+    print(color("info: ", "blue", attrs=['bold']), end='')
     print(msg)
 
 def warn(msg):
-    print(color("WARNING: ", "yellow", attrs=['bold']), end='')
+    print(color("warning: ", "yellow", attrs=['bold']), end='')
     print(msg)
 
 def error(msg):
-    print(color("ERROR: ", "red", attrs=['bold']), end='')
+    print(color("error: ", "red", attrs=['bold']), end='')
     print(msg)
 
 def errorq(msg):
-    print(color("CRITICAL: ", "red"), end='')
+    print(color("critical: ", "red"), end='')
     print(msg)
