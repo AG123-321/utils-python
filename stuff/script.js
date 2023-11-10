b5d21307ececb4bb04a983e6176dcb9688509dc8
AG123-321 <123011074+AG123-321@users.noreply.github.com>
2023-11-10 09:42:26 +0000

    some commit

--- stuff/script.js
+++ stuff/script.js
@@ -1,6 +1,9 @@
-window.onload = async function() {
+//window.onload = async function() {
     response = await fetch("https://api.github.com/repos/AG123-321/simple-bot/commits/main")
     json = await response.json()
     alert(json['commit']['tree'].url)
     alert("hi")
+//}
+window.onload = function() {
+    
 }
